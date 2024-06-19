5777e7468bf1e3aaa8831c94375038a2017d6f89
FjrSurya <fajar.suryantoro72@smk.belajar.id>
2024-06-19 11:59:06 +0700

    first push

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Barang;
+use App\Models\Kategori;
+use App\Models\Brmasuk;
+use App\Models\Brkeluar;
+use App\Models\Search;
+
+
+class DashboardController extends Controller
+{
+    public function index() {
+        return view('v_dashboard.index');
+    }
+}
